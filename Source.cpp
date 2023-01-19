6b766159e24a2789a1453baf9993cf6bb807418d
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2023-01-19 14:44:43 +0300

    Update

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,8 @@
 #include <iostream>
 
+// https://github.com/ArinaMonicheva/SeaBattle
+
 using namespace std;
-
-int main() {
-	int size = 10;
-	int** playersField = new int*[size];
-	int** AIField = new int*[size];
-
-	for (int i = 0; i < size; i++) {
-		playersField[i] = new int[size];
-		AIField[i] = new int[size];
-	}
-
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			playersField[i][j] = 0;
-		}
-	}
-
-	for (int i = 0; i < size; i++) {
-		delete [] playersField[i];
-		delete[] AIField[i];
-	}
-
-	delete[] playersField;
-	delete[] AIField;
-
-	return 0;
-}
 
 void drawField(int** field, int size) {
 
@@ -43,6 +18,28 @@ void drawField(int** field, int size) {
 			case 2:
 				cout << 'x';
 				break;
+			case 3:
+				cout << 'O';
+				break;
+			}
+		}
+	}
+
+	return;
+}
+
+void noRepeatRead(int** field, int loops, int decks) {
+
+	int X, Y;
+
+	for (int i = 0; i < loops; i++) {
+		cout << "Enter deck coords for" << i << "-deck ship" << endl;
+		int j = 0;
+		while (j < decks) {
+			cin >> X >> Y;
+			if (X > 1 && X < 11 && Y > 1 && Y < 11 && field[Y][X] != 1) {
+				field[Y - 1][X - 1] = 1;
+				j++;
 			}
 		}
 	}
@@ -63,17 +60,73 @@ void fillField(int** field, int size) {
 	return;
 }
 
-void noRepeatRead(int** field, int loops, int decks) {
-
+void gameLogic(int** playersField, int** AIField) {
+	bool gameOn = true;
 	int X, Y;
 
-	for (int i = 0; i < loops; i++) {
-		cout << "Enter deck coords for" << i << "-deck ship" << endl;
-		for (int j = 0; j < decks; j++) {
+	while (gameOn) {
+		cout << "Enter coords to shoot" << endl;
+		bool incorrect = true;
+		while (incorrect) {
 			cin >> X >> Y;
-			field[Y][X] = 1;
+			if (X > 1 && X < 11 && Y > 1 && Y < 11 && AIField[Y][X] != 3 && AIField[Y][X] != 2) {
+				switch (AIField[Y - 1][X - 1]) {
+				case 0:
+					cout << "You missed" << endl;
+					AIField[Y - 1][X - 1] = 3;
+					incorrect = false;
+					break;
+				case 1:
+					int untouchedDeck = 0;
+					for (int i = Y - 2; i < Y; i++) {
+						for (int j = X - 2; j < X; j++) {
+							if (AIField[Y - 1][X - 1] == 1) {
+								untouchedDeck++;
+							}
+						}
+					}
+
+					if (untouchedDeck) {
+						cout << "Hit!" << endl;
+					}
+					else {
+						cout << "Sinked!" << endl;
+					}
+
+					AIField[Y - 1][X - 1] = 2;
+					break;
+				};
+			}
+			else {
+				cout << "Wrong coord, try again!" << endl;
+			}
+		}
+	}
+}
+
+int main() {
+	int size = 10;
+	int** playersField = new int* [size];
+	int** AIField = new int* [size];
+
+	for (int i = 0; i < size; i++) {
+		playersField[i] = new int[size];
+		AIField[i] = new int[size];
+	}
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			playersField[i][j] = 0;
 		}
 	}
 
-	return;
+	for (int i = 0; i < size; i++) {
+		delete[] playersField[i];
+		delete[] AIField[i];
+	}
+
+	delete[] playersField;
+	delete[] AIField;
+
+	return 0;
 }
